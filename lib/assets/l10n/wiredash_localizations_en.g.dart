504355839d93d7fe481824c68176287947492b1c
Tanmoy karmakar <tanmoy.karmakar@saaslabs.co>
2023-06-14 23:13:15 +0530

    updated the required text

--- lib/assets/l10n/wiredash_localizations_en.g.dart
+++ lib/assets/l10n/wiredash_localizations_en.g.dart
@@ -11,19 +11,16 @@ class WiredashLocalizationsEn extends WiredashLocalizations {
   String get feedbackStep1MessageBreadcrumbTitle => 'Compose message';
 
   @override
-  String get feedbackStep1MessageDescription =>
-      'Add a short description of what you encountered';
+  String get feedbackStep1MessageDescription => 'Please add a short description';
 
   @override
-  String get feedbackStep1MessageHint =>
-      'There\'s an unknown error when I try to change my avatar...';
+  String get feedbackStep1MessageHint => '';
 
   @override
   String get feedbackStep1MessageErrorMissingMessage => 'Please add a message';
 
   @override
-  String get feedbackStep2LabelsTitle =>
-      'Which label best represents your feedback?';
+  String get feedbackStep2LabelsTitle => 'Which label best represents your feedback?';
 
   @override
   String get feedbackStep2LabelsBreadcrumbTitle => 'Labels';
@@ -33,29 +30,25 @@ class WiredashLocalizationsEn extends WiredashLocalizations {
       'Selecting the right category helps us identify the issue and route your feedback to the correct stakeholder';
 
   @override
-  String get feedbackStep3ScreenshotOverviewTitle =>
-      'Add screenshots for better context?';
+  String get feedbackStep3ScreenshotOverviewTitle => 'Please add a screenshot';
 
   @override
   String get feedbackStep3ScreenshotOverviewBreadcrumbTitle => 'Screenshots';
 
   @override
-  String get feedbackStep3ScreenshotOverviewDescription =>
-      'You\'ll be able to navigate the app and choose when to take a screenshot';
+  String get feedbackStep3ScreenshotOverviewDescription => 'You can choose where to take the screenshot';
 
   @override
   String get feedbackStep3ScreenshotOverviewSkipButton => 'Skip';
 
   @override
-  String get feedbackStep3ScreenshotOverviewAddScreenshotButton =>
-      'Add screenshot';
+  String get feedbackStep3ScreenshotOverviewAddScreenshotButton => 'Add screenshot';
 
   @override
   String get feedbackStep3ScreenshotBarNavigateTitle => 'Take a screenshot';
 
   @override
-  String get feedbackStep3ScreenshotBarDrawTitle =>
-      'Draw on screen to add highlights';
+  String get feedbackStep3ScreenshotBarDrawTitle => 'Draw on screen to add highlights';
 
   @override
   String get feedbackStep3ScreenshotBarDrawUndoButton => 'Undo';
@@ -86,12 +79,10 @@ class WiredashLocalizationsEn extends WiredashLocalizations {
   String get feedbackStep4EmailBreadcrumbTitle => 'Contact';
 
   @override
-  String get feedbackStep4EmailDescription =>
-      'Add your email address below or leave empty';
+  String get feedbackStep4EmailDescription => 'Add your email address below or leave empty';
 
   @override
-  String get feedbackStep4EmailInvalidEmail =>
-      'This doesn\'t look like a valid email address. You can leave it empty.';
+  String get feedbackStep4EmailInvalidEmail => 'This doesn\'t look like a valid email address. You can leave it empty.';
 
   @override
   String get feedbackStep4EmailInputHint => 'mail@example.com';
@@ -119,20 +110,16 @@ class WiredashLocalizationsEn extends WiredashLocalizations {
   String get feedbackStep6SubmitSubmitDetailsTitle => 'Feedback details';
 
   @override
-  String get feedbackStep7SubmissionInFlightMessage =>
-      'Submitting your feedback';
+  String get feedbackStep7SubmissionInFlightMessage => 'Submitting your feedback';
 
   @override
-  String get feedbackStep7SubmissionSuccessMessage =>
-      'Thanks for your feedback!';
+  String get feedbackStep7SubmissionSuccessMessage => 'Thanks for your feedback!';
 
   @override
-  String get feedbackStep7SubmissionErrorMessage =>
-      'Feedback submission failed';
+  String get feedbackStep7SubmissionErrorMessage => 'Feedback submission failed';
 
   @override
-  String get feedbackStep7SubmissionOpenErrorButton =>
-      'Click to see error details';
+  String get feedbackStep7SubmissionOpenErrorButton => 'Click to see error details';
 
   @override
   String get feedbackStep7SubmissionRetryButton => 'Retry';
@@ -158,16 +145,13 @@ class WiredashLocalizationsEn extends WiredashLocalizations {
   String get feedbackCloseButton => 'Close';
 
   @override
-  String get promoterScoreStep1Question =>
-      'How likely are you to recommend us?';
+  String get promoterScoreStep1Question => 'How likely are you to recommend us?';
 
   @override
-  String get promoterScoreStep1Description =>
-      '0 = Not likely, 10 = Most likely';
+  String get promoterScoreStep1Description => '0 = Not likely, 10 = Most likely';
 
   @override
-  String get promoterScoreStep2MessageTitle =>
-      'How likely are you to recommend us to your friends and family?';
+  String get promoterScoreStep2MessageTitle => 'How likely are you to recommend us to your friends and family?';
 
   @override
   String promoterScoreStep2MessageDescription(int rating) {
@@ -175,20 +159,16 @@ class WiredashLocalizationsEn extends WiredashLocalizations {
   }
 
   @override
-  String get promoterScoreStep2MessageHint =>
-      'It would be great if you could improve...';
+  String get promoterScoreStep2MessageHint => 'It would be great if you could improve...';
 
   @override
-  String get promoterScoreStep3ThanksMessagePromoters =>
-      'Thanks for your rating!';
+  String get promoterScoreStep3ThanksMessagePromoters => 'Thanks for your rating!';
 
   @override
-  String get promoterScoreStep3ThanksMessagePassives =>
-      'Thanks for your rating!';
+  String get promoterScoreStep3ThanksMessagePassives => 'Thanks for your rating!';
 
   @override
-  String get promoterScoreStep3ThanksMessageDetractors =>
-      'Thanks for your rating!';
+  String get promoterScoreStep3ThanksMessageDetractors => 'Thanks for your rating!';
 
   @override
   String get promoterScoreNextButton => 'Next';
